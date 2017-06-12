b3e733446d15b9bdb1c6ca94ecee96c10709a9a3
bryanchapman9999 <bryan.d.chapman@gmail.com>
2017-06-12 11:47:41 +0100

    AD User Add

---
+++ NewSPS_User.ps1
@@ -0,0 +1,13 @@
+﻿Import-Module ActiveDirectory -ErrorAction SilentlyContinue
+
+$firstname = "Amanda"
+$surname = "Moody"
+$office = "Neptune House"
+$samaccountname = "$firstname.$surname"
+$logonscript = "Logon.bat"
+
+New-ADUser -AccountPassword (ConvertTo-SecureString “SPSjun17” -AsPlainText -Force) -Enabled $true `
+-Name "$firstname $surname" -SamAccountName $samaccountname -GivenName $firstname -Surname $surname `
+-DisplayName "$firstname $surname" -UserPrincipalName ($samaccountname + '@spseu.com') -ScriptPath $logonscript `
+-HomeDrive H: -HomeDirectory "\\spssrv03\users\$($samaccountname)" -EmailAddress ($samaccountname + '@spseu.com') `
+-office $office 
